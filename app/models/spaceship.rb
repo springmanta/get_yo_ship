d91a1e970fea743aa1d9319450de72eced858587
miguel rodrigues <mike_slx@yahoo.com>
2025-03-06 14:42:09 +0000

    added date range fuctionality

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,6 +1,8 @@
 class Spaceship < ApplicationRecord
   has_one_attached :photo
+  has_many :availabilities, dependent: :destroy
   include PgSearch::Model
+  attr_accessor :is_available
 
   pg_search_scope :search_by_details,
                   against: [:name, :capacity, :description],
---
+++ db/migrate/20250306121113_create_availabilities.rb
@@ -0,0 +1,11 @@
+class CreateAvailabilities < ActiveRecord::Migration[7.1]
+  def change
+    create_table :availabilities do |t|
+      t.references :spaceship, null: false, foreign_key: true
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps
+    end
+  end
+end
