658444c8f069d7331d077d06e8a58a93b7ee16ea
Simao Martins <simaopmartins@gmail.com>
2025-03-06 12:42:54 +0000

    improved spaceship model and created new seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_04_142826) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_06_121808) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_04_142826) do
     t.boolean "availability"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "made_by"
+    t.string "ship_class"
+    t.float "max_speed"
   end
 
   create_table "users", force: :cascade do |t|
